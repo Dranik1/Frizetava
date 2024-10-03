fef9f0168109d36af99ee131778ea0b96739bcec
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-10-03 14:35:28 +0300

    h

--- proba.py
+++ proba.py
@@ -1,9 +1,10 @@
 
 Dalibnieka_dzimums="s"
-Dalibnieka_svars=51
+Dalibnieka_svars=68
 
 
 kategorijas_siev=[51, 56, 62, 69, 300]
+kategorijas_siev_name=["-50 kg", "51-55 kg", "56-61 kg", "62-68 kg", "69+ kg"]
 '''if Dalibnieka_dzimums[0]=="s":
     for i in range(6):
         a=0
@@ -20,11 +21,11 @@ b=1
 c=1
 while stop!=True:
     if Dalibnieka_svars in range(1, kategorijas_siev[0]):
-        print(c)
+        print(kategorijas_siev_name[c-1])
         stop=True
     elif Dalibnieka_svars in range(kategorijas_siev[a], kategorijas_siev[b]):
         c+=1
-        print(c)
+        print(kategorijas_siev_name[c-1])
         stop=True
     else:
         a=a+1
--- veikals.py
+++ veikals.py
@@ -35,9 +35,14 @@ class Dalibnieks:
     
 
 class Kumite(Dalibnieks):
+    def __init__(self, vards, uzvards, dzimums, vecums, svars, josta, pk):
+        super().__init__(vards, uzvards, dzimums, vecums, svars, josta, pk)
     kategorijas_vir=[61, 68, 76, 85, 300]
+    kategorijas_vir_name=["-60 kg", "61-67 kg", "68-75 kg", "76-84 kg", "85+ kg"]
     kategorijas_siev=[51, 56, 62, 69, 300]
+    kategorijas_siev_name=["-50 kg", "51-55 kg", "56-61 kg", "62-68 kg", "69+ kg"]
     def svara_kategorija(self):
+        self.Dalibnieka_svara_kategorija=""
         if self.Dalibnieka_dzimums[0]=="s":
             stop=False
             a=0
@@ -45,11 +50,13 @@ class Kumite(Dalibnieks):
             c=1
             while stop!=True:
                 if self.Dalibnieka_svars in range(1, self.kategorijas_siev[0]):
-                    print(c)
+                    print(self.kategorijas_siev_name[c-1])
+                    self.Dalibnieka_svara_kategorija=self.kategorijas_siev_name[c-1]
                     stop=True
                 elif self.Dalibnieka_svars in range(self.kategorijas_siev[a], self.kategorijas_siev[b]):
                     c+=1
-                    print(c)
+                    print(self.kategorijas_siev_name[c-1])
+                    self.Dalibnieka_svara_kategorija=self.kategorijas_siev_name[c-1]
                     stop=True
                 else:
                     a=a+1
@@ -62,11 +69,13 @@ class Kumite(Dalibnieks):
             c=1
             while stop!=True:
                 if self.Dalibnieka_svars in range(1, self.kategorijas_vir[0]):
-                    print(c)
+                    print(self.kategorijas_vir_name[c-1])
+                    self.Dalibnieka_svara_kategorija=self.kategorijas_vir_name[c-1]
                     stop=True
                 elif self.Dalibnieka_svars in range(self.kategorijas_vir[a], self.kategorijas_vir[b]):
                     c+=1
-                    print(c)
+                    print(self.kategorijas_vir_name[c-1])
+                    self.Dalibnieka_svara_kategorija=self.kategorijas_vir_name[c-1]
                     stop=True
                 else:
                     a=a+1
@@ -74,7 +83,28 @@ class Kumite(Dalibnieks):
                     c+=1
         else:
             print("Who are you?")
-            
+    def registration(self):
+        if self.Dalibnieka_dzimums[0]=="v":
+            self.gender="virietis"
+        elif self.Dalibnieka_dzimums[0]=="s":
+            self.gender="sieviete"
+        else:
+            self.gender=None
+        dalibnieka_data={
+            "name":self.Dalibnieka_vards,
+            "surname":self.Dalibnieka_uzvards,
+            "gender":self.gender,
+            "age":self.Dalibnieka_vecums,
+            "weight":self.Dalibnieka_svars,
+            "belt":self.Dalibnieka_josta,
+            "personal code":self.Dalibnieka_pk,
+            "weight cathegory":self.Dalibnieka_svara_kategorija
+        }
+
+        with open("dalibnieki.json", mode="w", encoding="utf-8") as write_file:
+            json.dump(dalibnieka_data, write_file)
+
+
 class Kata(Dalibnieks):
     pass
 
@@ -85,5 +115,12 @@ dal1=Dalibnieks("anna", "h", "sieviete", 17, 45, "3 Kyu", "123456-78900")
 dal1.dalibnieka_info()
 dal1=Kumite("anna", "h", "sieviete", 17, 45, "3 Kyu", "123456-78900")
 dal1.svara_kategorija()
+dal1.registration()
+
+dal2=Dalibnieks("anna", "h", "v", 17, 45, "3 Kyu", "123456-78900")
+dal2.dalibnieka_info()
+dal2=Kumite("viktor", "h", "v", 17, 199, "3 Kyu", "123456-78900")
+dal2.svara_kategorija()
+dal2.registration()
 
 
